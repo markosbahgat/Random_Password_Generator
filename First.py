1a137b22409c90d9c2e3f55560fad3c52b105469
markosbahgat <markos.bahgat@gmail.com>
2021-06-28 11:44:08 +0200

    First Commit

---
+++ First.py
@@ -0,0 +1,107 @@
+#mylist = [1,2,3,6,5,6,7,8,9]
+#print(len(mylist))
+
+
+    #print("اي هيا الصفة المنطبقة علي شهر ميلادك ؟!")
+
+#variable set to make a rondom words app
+#Setone = {"قرد", "حمار", "كلب", "جاموسة", "حيوان شمال", "متخلف عقلياً","زبالة","زابلة","حشرة"}
+
+#print(Setone.pop())
+
+
+
+
+
+
+
+
+
+
+                                    #random password generator
+
+
+
+
+
+
+Passwordset = {"A","B","C","D","E","S","F","G","H","J","K","L","Z","X","V","N","M","Q","W","R","T","Y","U","I","O","P","1","2","3","4","5","6","7","8","9","0","/","*","-","+","!","@","#","$","^","&","%"}
+Passwordlist = ["A","B","C","D","E","S","F","G","H","J","K","L","Z","X","V","N","M","Q","W","R","T","Y","U","I","O","P"]
+#str1 = ("")
+fuck = ['a', 'b', 'c', 'd', 'e', 's', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'v', 'n', 'm', 'q', 'w', 'r', 't', 'y', 'u', 'i', 'o', 'p']
+#str1.join(Passwordlist)
+#print(str1.join(Passwordlist))
+#print(str1.join(Passwordlist).swapcase().split())
+#print(fuck.split( ))
+#a = Passwordset.pop()
+#b = Passwordset.pop()
+#c = Passwordset.pop()
+#d = Passwordset.pop()
+#e = Passwordset.pop()
+#f = Passwordset.pop()
+#g = Passwordset.pop()
+#h = Passwordset.pop()
+#i = Passwordset.pop()
+#g = Passwordset.pop()
+#k = Passwordset.pop()
+#m = Passwordset.pop()
+#n = Passwordset.pop()
+#l = Passwordset.pop()
+#o = Passwordset.pop()
+#p = Passwordset.pop()
+
+x = 10
+mylist = []
+while x > 0 :
+    Passwordset.pop()
+    fuck.pop()
+    mylist.append(Passwordset.pop())
+    mylist.append(fuck.pop())
+    x -= 1
+
+
+string1= ("")
+string1.join(mylist)
+print(string1.join(mylist))
+print(len(string1.join(mylist)))
+
+
+
+
+#================================================================
+#a = fuck.pop()
+#b = fuck.pop()
+#c = fuck.pop()
+#d = fuck.pop()
+#e = fuck.pop()
+#f = fuck.pop()
+
+#===============================================================
+#list1 = []
+#list1.append(a)
+#list1.append(b)
+#list1.append(c)
+#list1.append(d)
+#list1.append(e)
+#list1.append(f)
+#list1.append(g)
+#list1.append(h)
+#list1.append(i)
+#list1.append(g)
+#list1.append(k)
+    #list1.append(m)
+    #list1.append(n)
+#list1.append(l)
+#list1.append(o)
+#list1.append(p)
+#
+#list1.append(a)
+#list1.append(b)
+#list1.append(c)
+#list1.append(d)
+
+
+    #string2 = ("")
+    #print(string2.join(list1))
+#print(list1.__add__(a))
+#print(list1)
