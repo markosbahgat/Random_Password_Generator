2e6586a41e4f0a08086ab2a2872b6c3859b304c9
Markos Bahgat <56962387+markosbahgat@users.noreply.github.com>
2021-06-28 11:50:12 +0200

    Update First.py

--- First.py
+++ First.py
@@ -1,24 +1,9 @@
-#mylist = [1,2,3,6,5,6,7,8,9]
-#print(len(mylist))
-
-
-    #print("اي هيا الصفة المنطبقة علي شهر ميلادك ؟!")
-
-#variable set to make a rondom words app
-#Setone = {"قرد", "حمار", "كلب", "جاموسة", "حيوان شمال", "متخلف عقلياً","زبالة","زابلة","حشرة"}
-
-#print(Setone.pop())
 
 
 
 
 
-
-
-
-
-
-                                    #random password generator
+                                                                     #random password generator
 
 
 
@@ -27,8 +12,26 @@
 
 Passwordset = {"A","B","C","D","E","S","F","G","H","J","K","L","Z","X","V","N","M","Q","W","R","T","Y","U","I","O","P","1","2","3","4","5","6","7","8","9","0","/","*","-","+","!","@","#","$","^","&","%"}
 Passwordlist = ["A","B","C","D","E","S","F","G","H","J","K","L","Z","X","V","N","M","Q","W","R","T","Y","U","I","O","P"]
-#str1 = ("")
 fuck = ['a', 'b', 'c', 'd', 'e', 's', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'v', 'n', 'm', 'q', 'w', 'r', 't', 'y', 'u', 'i', 'o', 'p']
+
+
+x = 10
+mylist = []
+while x > 0 :
+    Passwordset.pop()
+    fuck.pop()
+    mylist.append(Passwordset.pop())
+    mylist.append(fuck.pop())
+    x -= 1
+
+
+string1= ("")
+string1.join(mylist)
+print(string1.join(mylist))
+print(len(string1.join(mylist)))
+
+
+#================================================================
 #str1.join(Passwordlist)
 #print(str1.join(Passwordlist))
 #print(str1.join(Passwordlist).swapcase().split())
@@ -49,24 +52,6 @@ fuck = ['a', 'b', 'c', 'd', 'e', 's', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'v
 #l = Passwordset.pop()
 #o = Passwordset.pop()
 #p = Passwordset.pop()
-
-x = 10
-mylist = []
-while x > 0 :
-    Passwordset.pop()
-    fuck.pop()
-    mylist.append(Passwordset.pop())
-    mylist.append(fuck.pop())
-    x -= 1
-
-
-string1= ("")
-string1.join(mylist)
-print(string1.join(mylist))
-print(len(string1.join(mylist)))
-
-
-
 
 #================================================================
 #a = fuck.pop()
@@ -105,3 +90,15 @@ print(len(string1.join(mylist)))
     #print(string2.join(list1))
 #print(list1.__add__(a))
 #print(list1)
+
+
+#mylist = [1,2,3,6,5,6,7,8,9]
+#print(len(mylist))
+
+
+    #print("اي هيا الصفة المنطبقة علي شهر ميلادك ؟!")
+
+#variable set to make a rondom words app
+#Setone = {"قرد", "حمار", "كلب", "جاموسة", "حيوان شمال", "متخلف عقلياً","زبالة","زابلة","حشرة"}
+
+#print(Setone.pop())
